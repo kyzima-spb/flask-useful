c2e54d2413f43515f79479dc610f67502089fe2f
Kirill Vercetti <office@kyzima-spb.com>
2020-07-26 18:39:03 +0300

    Добавлен валидатор, который проверяет, что значение было введено только в одном из указанных полей формы.

--- flask_useful/wtforms/validators.py
+++ flask_useful/wtforms/validators.py
@@ -4,6 +4,37 @@ import phonenumbers
 from wtforms.validators import ValidationError, StopValidation
 
 
+class OnlyOne(object):
+    """
+    Validates that a value was entered in only one of the specified form fields.
+    """
+
+    def __init__(self, fieldnames, message=None):
+        self.fieldnames = fieldnames.split() if isinstance(fieldnames, str) else fieldnames
+        self.message = message
+
+    def __call__(self, form, field):
+        fieldnames = [field.name, *self.fieldnames]
+
+        try:
+            values = [bool(form[i].data) for i in fieldnames]
+        except KeyError as err:
+            field_name, *_ = err.args
+            raise ValidationError(field.gettext("Invalid field name '%s'.") % field_name)
+
+        only_one = values.count(True) == 1
+
+        if not only_one:
+            message = self.message
+            if message is None:
+                message = field.gettext('It is required to set the value of one of the fields: %(fieldnames)s.')
+
+            raise ValidationError(message % {'fieldnames': ', '.join(fieldnames)})
+
+        if only_one and not field.data:
+            raise StopValidation
+
+
 class Phone(object):
     def __init__(self, region=None, keep_raw_input=False, _check_region=True, message=None):
         self.region = region
@@ -22,7 +53,7 @@ class Phone(object):
             raise ValidationError(err)
 
 
-class SlugValidator(object):
+class Slug(object):
     slug_re = r'^[a-z0-9-_]+$'
     slug_unicode_re = r'^[-\w]+$'
 
