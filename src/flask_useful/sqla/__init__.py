c32aca3b790b35e0d8b9b6f589cf2a47784b7fb0
Kirill Vercetti <office@kyzima-spb.com>
2023-06-05 14:45:22 +0300

    sql module to package

---
+++ src/flask_useful/sqla/__init__.py
@@ -0,0 +1,11 @@
+from .session import *
+from .utils import *
+
+
+__all__ = (
+    'generate_slug',
+    'get_sqla_session',
+    'normalize_pk',
+    'sqla_session',
+    'SessionMixin',
+)
--- src/flask_useful/sqla/utils.py
+++ src/flask_useful/sqla/utils.py
@@ -1,28 +1,18 @@
 from __future__ import annotations
-from types import TracebackType
 import typing as t
 import re
 
-from flask import current_app
 import sqlalchemy as sa
 from sqlalchemy.orm import Session
-from werkzeug.local import LocalProxy
 
+from .session import sqla_session
 
 IdentityArgument = t.Union[t.Any, t.Tuple[t.Any, ...], t.Dict[str, t.Any]]
 
 
 __all__ = (
     'generate_slug',
-    'get_sqla_session',
     'normalize_pk',
-    'sqla_session',
-)
-
-
-sqla_session = t.cast(
-    Session,
-    LocalProxy(lambda: get_sqla_session()),
 )
 
 
@@ -40,7 +30,7 @@ def generate_slug(
         session (Session): SQLAlchemy session.
     """
     if session is None:
-        session = get_sqla_session()
+        session = sqla_session
 
     pattern = r'^%s(?:-([0-9]+))?$' % slug
 
@@ -63,19 +53,6 @@ def generate_slug(
     return '{}-{}'.format(slug, int(match.group(1)) + 1)
 
 
-def get_sqla_session() -> Session:
-    """Returns the current session instance from application context."""
-    ext = current_app.extensions.get('sqlalchemy')
-
-    if ext is None:
-        raise RuntimeError(
-            'An extension named sqlalchemy was not found '
-            'in the list of registered extensions for the current application.'
-        )
-
-    return t.cast(Session, ext.db.session)
-
-
 def normalize_pk(
     value: IdentityArgument,
     model_class: t.Type[t.Any],
@@ -94,27 +71,3 @@ def normalize_pk(
     return {
         c.name: c.type.python_type(v) for c, v in zip(columns, value)
     }
-
-
-class SessionMixin:
-    """
-    The mixin adds a property with the current session
-    and an auto-commit context manager.
-    """
-
-    def __enter__(self) -> Session:
-        return self.session
-
-    def __exit__(
-        self,
-        err_type: t.Optional[t.Type[BaseException]],
-        err: t.Optional[BaseException],
-        traceback: t.Optional[TracebackType]
-    ) -> t.Optional[bool]:
-        if err is None:
-            self.session.commit()
-        return None
-
-    @property
-    def session(self) -> Session:
-        return sqla_session
