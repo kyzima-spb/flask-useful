b26ee8f03f659aa5b0d2a6179fd31e5477973eff
Kirill Vercetti <office@kyzima-spb.com>
2023-09-22 16:48:18 +0300

    Added Email type for Click

--- src/flask_useful/cli.py
+++ src/flask_useful/cli.py
@@ -3,9 +3,17 @@ import typing as t
 
 import click
 
+if t.TYPE_CHECKING:
+    import email_validator
+else:
+    try:
+        import email_validator
+    except ImportError:
+        email_validator = None
+
 
 __all__ = (
-    'Choice',
+    'Choice', 'Email',
 )
 
 
@@ -64,3 +72,26 @@ class Choice(click.Choice):
             return str(value[self.field_name])
         else:
             return str(getattr(value, self.field_name))
+
+
+class Email(click.ParamType):
+    name = 'email'
+
+    def __init__(self, **options: t.Any) -> None:
+        self.options = options
+
+    def convert(
+        self,
+        value: str,
+        param: t.Optional[click.Parameter],
+        ctx: t.Optional[click.Context],
+    ) -> str:
+        if email_validator is None:
+            raise RuntimeError('The email-validator package must be installed.')
+
+        try:
+            email_validator.validate_email(value, **self.options)
+        except email_validator.EmailNotValidError as err:
+            self.fail(str(err), param, ctx)
+
+        return value
